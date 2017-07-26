52823a75dda6e303a899cdb65d1fb4e5319a7301
moritaku-92 <moritaku00eva@gmail.com>
2017-07-26 22:54:46 +0900

    修正、コード追加

--- skillgroup_cc2/skillgroup_cc2.go
+++ skillgroup_cc2/skillgroup_cc2.go
@@ -32,37 +32,38 @@ func(t * SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 
 }
 
+// invoke処理 functionによって行う処理を変えるß
 func(t * SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	logger.Info("########### skill group cc2 Invoke ###########")
 	
 	fn, args := stub.GetFunctionAndParameters()
 
-	// 依頼を出す
+	// 任務依頼
 	if fn == "request" {
 		return t.order(stub, args)
 	}
 
-	// 依頼の削除
+	// 任務削除
 	if fn == "delete" {
 		return t.delete(stub, args)
 	}
 
-	// 依頼を受ける
+	// 任務受注
 	if fn == "receive" {
 		return t.receive(stub, args)
 	}
 
-	// 依頼の取り消し
+	// 任務取消
 	if fn == "cancel" {
 		return t.cancel(stub, args)
 	}
 
-	// 依頼の完了
+	// 任務完了
 	if fn == "complete" {
 		return t.complete(stub, args)
 	}
 
-	// 依頼一覧取得
+	// 任務一覧取得
 	if fn == "query" {
 		return t.query(stub, args)
 	}
@@ -72,25 +73,34 @@ func(t * SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	return shim.Error(fmt.Sprintf("Unknown action, check the first argument, must be one of 'delete', 'query', or 'move'. But got: %v", args[0]))
 }
 
-// 依頼を出す
+// 任務を出す
 func(t * SimpleChaincode) request(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	logger.Info("########### request ###########")
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
+	// 受け取ったjson
 	A := args[0]
 
-	count, err := stub.GetState("count")
-	if err != nil {
-		return shim.Error("依頼番号取れなかった")
-	}
-	count = count + 1;
+	/*
+		jsonの形どうしよう…？
+		正しいjsonか判定してたら大変…
+	*/
+	/*
+		{
+			"任務者":"a",
+			"受領有無":0,
+			"任務内容":"○○○買ってこいや",
+			"報酬":10000,
+			"受注者":""
+		}
+	*/
 
 	// クエスト番号設定
 	quest := "quest" + count
 
-	// 依頼の登録
+	// 任務の登録
 	err = stub.PutState(count, []byte(strconv.Itoa(A)))
 	if err != nil {
 		return shim.Error(err.Error())
@@ -105,22 +115,43 @@ func(t * SimpleChaincode) request(stub shim.ChaincodeStubInterface, args []strin
 	return shim.Success(nil)
 }
 
-// 依頼を削除する
+// 任務削除
 func(t * SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	logger.Info("########### delete ###########")
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
-	// 依頼番号が着ている想定{quest{N}}
+	// 任務番号がきている想定{quest{N}}
 	A := args[0]
+	// 番号が正しいか判定入れる？
 
-	// 依頼削除
+	// 任務削除
 	err := stub.DelState(A)
 	if err != nil {
 		return shim.Error("Failed to delete state")
 	}
 
+	return shim.Success(nil)
+}
+
+// 任務受注
+func(t * SimpleChaincode) receive(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+// 任務取消
+func(t * SimpleChaincode) cancel(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+// 任務取消
+func(t * SimpleChaincode) complete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+// 任務一覧取得
+func(t * SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	return shim.Success(nil)
 }
 
