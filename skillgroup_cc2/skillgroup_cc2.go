ce81c098132a2665f03017975c31df78d357d36b
KitagawaYuki <ykitagawa@dcs.co.jp>
2017-09-14 22:49:58 +0900

    [2017/09/14]エラー対応 ①Err型をＳｔｒｉｎｇ型に変換（Err.Error()) ②ＣＣ1を呼び出す際のチェーンコード名修正（mycc→skillgroup_cc1）

--- skillgroup_cc2/skillgroup_cc2.go
+++ skillgroup_cc2/skillgroup_cc2.go
@@ -203,7 +203,7 @@ func(t * SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string
 	// 任務番号があるか判定
 	_, err1 := stub.GetState(missinoNo)
 	if err1 != nil {
-		return shim.Error(err)
+		return shim.Error(err1.Error())
 	}
 
 	// 任務削除
@@ -234,7 +234,7 @@ func(t * SimpleChaincode) receive(stub shim.ChaincodeStubInterface, args []strin
 	missionCon, err := stub.GetState(missionNo)
 	if err != nil {
 		// 番号がなければエラーを返す
-		return shim.Error(err)
+		return shim.Error(err.Error())
 	}
 
 	// 依頼者
@@ -246,7 +246,7 @@ func(t * SimpleChaincode) receive(stub shim.ChaincodeStubInterface, args []strin
 	var mission = Mission{}
 	err0 := json.Unmarshal(missionCon, &mission)
 	if err0 != nil {
-		return shim.Error(err0)
+		return shim.Error(err0.Error())
 	}
 
 	// 受注者の登録
@@ -256,7 +256,7 @@ func(t * SimpleChaincode) receive(stub shim.ChaincodeStubInterface, args []strin
 	// jsonエンコード
 	outputJSON, err := json.Marshal(&mission)
 	if err != nil {
-		return shim.Error("json化できなかった",err)
+		return shim.Error(err.Error())
 	}
 
 	// 登録
@@ -287,14 +287,14 @@ func(t * SimpleChaincode) cancel(stub shim.ChaincodeStubInterface, args []string
 	missionCon, err := stub.GetState(missionNo)
 	if err != nil {
 		// 番号がなければエラーを返す
-		return shim.Error(err)
+		return shim.Error(err.Error())
 	}
 
 	// 任務を取得し構造体にぶっ込む
 	var mission = Mission{}
 	err0 := json.Unmarshal(missionCon, &mission)
 	if err0 != nil {
-		return shim.Error(err0)
+		return shim.Error(err0.Error())
 	}
 
 	// 任務の取り消し
@@ -304,7 +304,7 @@ func(t * SimpleChaincode) cancel(stub shim.ChaincodeStubInterface, args []string
 	// jsonエンコード
 	outputJSON, err := json.Marshal(&mission)
 	if err != nil {
-		return shim.Error("json化できなかった",err)
+		return shim.Error(err.Error())
 	}
 
 	// 登録
@@ -335,14 +335,14 @@ func(t * SimpleChaincode) complete(stub shim.ChaincodeStubInterface, args []stri
 	missionCon, err := stub.GetState(missionNo)
 	if err != nil {
 		// 番号がなければエラーを返す
-		return shim.Error(err)
+		return shim.Error(err.Error())
 	}
 
 	// 任務を取得し構造体にぶっ込む
 	var mission = Mission{}
 	err0 := json.Unmarshal(missionCon, &mission)
 	if err0 != nil {
-		return shim.Error(err0)
+		return shim.Error(err0.Error())
 	}
 	
 	/*
@@ -353,7 +353,7 @@ func(t * SimpleChaincode) complete(stub shim.ChaincodeStubInterface, args []stri
 	invokeArgs := util.ToChaincodeArgs("move", mission.Requester, mission.Contractor, strconv.Itoa(mission.Compensation))
 	
 	// chaincodeName, json, channel で設定する↓
-	response := stub.InvokeChaincode("mycc", invokeArgs, "mychannel")
+	response := stub.InvokeChaincode("skillgroup_cc1", invokeArgs, "mychannel")
 
 	if response.Status != shim.OK {
 		errStr := fmt.Sprintf("Failed to invoke chaincode. Got error: %s", string(response.Payload))
@@ -367,7 +367,7 @@ func(t * SimpleChaincode) complete(stub shim.ChaincodeStubInterface, args []stri
 	// jsonエンコード
 	outputJSON, err := json.Marshal(&mission)
 	if err != nil {
-		return shim.Error("json化できなかった",err)
+		return shim.Error(err.Error())
 	}
 
 	// 登録
